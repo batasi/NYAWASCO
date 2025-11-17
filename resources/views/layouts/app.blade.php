1da644544edba28fe6d3d581045c19cd2eb9e2aa
solomon <batasisolomon029@gmail.com>
2025-11-17 11:39:37 +0300

    icons for social media

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -942,18 +942,7 @@ use Illuminate\Support\Facades\Route;
             </div>
 
             <!-- Social Links -->
-            <div class="mobile-social desktop-only">
-                <a href="#" aria-label="Facebook">
-                    <i class="fab fa-facebook-f"></i>
-                </a>
-                <a href="#" aria-label="X">
-                    <span style="font-weight: bold; font-size: 1.2em;">X</span>
-                </a>
 
-                <a href="#" aria-label="Search">
-                    <i class="fas fa-search"></i>
-                </a>
-            </div>
         </div>
     </div>
 
